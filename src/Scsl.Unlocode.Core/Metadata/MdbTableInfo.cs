f6abd8987b143588644e7e21fc60cfff8511e1e9
asparatu <asparatu@gmail.com>
2026-02-01 20:35:40 +0800

    feat(metadata): add MdbTableInfo

---
+++ src/Scsl.Unlocode.Core/Metadata/MdbTableInfo.cs
@@ -0,0 +1,7 @@
+﻿namespace Scsl.Unlocode.Core.Metadata;
+
+public sealed class MdbTableInfo
+{
+    public string Name { get; init; } = default!;
+    public string Type { get; init; } = default!;
+}
