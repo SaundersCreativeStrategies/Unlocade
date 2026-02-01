46f2ba284c628e4716650f778ee1fce4ad736239
asparatu <asparatu@gmail.com>
2026-02-01 20:48:00 +0800

    feat(rendering): add table renderer

---
+++ src/Unlocade.DataImporter/Presentation/MdbTableRenderer.cs
@@ -0,0 +1,23 @@
+﻿using Scsl.Unlocode.Core.Metadata;
+
+using Spectre.Console;
+
+namespace Unlocade.DataImporter.Presentation;
+
+public class MdbTableRenderer
+{
+    public void Render(IReadOnlyList<MdbTableInfo> tables)
+    {
+        var table = new Table()
+            .Title("[yellow]MDB Tables[/]")
+            .AddColumn("[cyan]Name[/]")
+            .AddColumn("[green]Type[/]");
+
+        foreach (var t in tables)
+        {
+            table.AddRow(t.Name, t.Type);
+        }
+
+        AnsiConsole.Write(table);
+    }
+}
