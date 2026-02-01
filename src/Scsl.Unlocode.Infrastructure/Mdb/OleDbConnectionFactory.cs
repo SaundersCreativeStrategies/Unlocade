18f9d7fa487619e8d3259d8d18c0a009f8764bbc
asparatu <asparatu@gmail.com>
2026-02-01 22:05:16 +0800

    feat(db): add verbose diagnostics logging to OleDbConnectionFactory

--- src/Scsl.Unlocode.Infrastructure/Mdb/OleDbConnectionFactory.cs
+++ src/Scsl.Unlocode.Infrastructure/Mdb/OleDbConnectionFactory.cs
@@ -1,17 +1,25 @@
 ﻿using System.Data.OleDb;
 
+using Scsl.Unlocode.Infrastructure.Diagnostics;
+
 namespace Scsl.Unlocode.Infrastructure.Mdb;
 
 public static class OleDbConnectionFactory
 {
     private const string Provider = "Microsoft.ACE.OLEDB.16.0";
 
-    public static OleDbConnection Create(string filePath)
+    public static OleDbConnection Create(string filePath, IDiagnosticsSink? diagnostics)
     {
         if(string.IsNullOrWhiteSpace(filePath))
             throw new ArgumentException("MDB path cannot be null or empty", nameof(filePath));
 
         var connectionString = $"Provider={Provider};Data Source={filePath};Persist Security Info=False;";
+
+        // Verbose logging
+        diagnostics?.LogInfo($"OLE DB Provdider: {Provider}");
+        diagnostics?.LogInfo($"MDB Path: {filePath}");
+        diagnostics?.LogInfo($"Creating OLE DB Connection");
+
         return new OleDbConnection(connectionString);
     }
 }
