28e21a6ef48d4c9d2f335d7b5037ee9406e0ad9d
asparatu <asparatu@gmail.com>
2026-02-01 21:46:23 +0800

    feat(infrastucture): add IDiagnosticsSink

---
+++ src/Scsl.Unlocode.Infrastructure/Diagnostics/IDiagnosticsSink.cs
@@ -0,0 +1,8 @@
+﻿namespace Scsl.Unlocode.Infrastructure.Diagnostics;
+
+public interface IDiagnosticsSink
+{
+    void Info(string message);
+    void Warn(string message);
+    void Error(string message);
+}
