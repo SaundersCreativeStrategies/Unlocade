3bd9fc2658ac36c4e68be94773f374cde015f1ff
asparatu <asparatu@gmail.com>
2026-02-01 21:40:11 +0800

    feat(ole-db): add OleDb connection factory

---
+++ src/Scsl.Unlocode.Infrastructure/Mdb/OleDbConnectionFactory.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.OleDb;
+
+namespace Scsl.Unlocode.Infrastructure.Mdb;
+
+public static class OleDbConnectionFactory
+{
+    private const string Provider = "Microsoft.ACE.OLEDB.16.0";
+
+    public static OleDbConnection Create(string filePath)
+    {
+        if(string.IsNullOrWhiteSpace(filePath))
+            throw new ArgumentException("MDB path cannot be null or empty", nameof(filePath));
+
+        var connectionString = $"Provider={Provider};Data Source={filePath};Persist Security Info=False;";
+        return new OleDbConnection(connectionString);
+    }
+}
