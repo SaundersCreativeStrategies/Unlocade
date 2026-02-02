90ebad327e7ffe28c300c168ae3d94205a9cb11a
asparatu <asparatu@gmail.com>
2026-02-02 22:13:46 +0800

    refactor(diagnostics)!: add exception parameter to LogError

---
+++ src/Scsl.Unlocode.Core/Diagnostics/IDiagnosticsSink.cs
@@ -0,0 +1,8 @@
+﻿namespace Scsl.Unlocode.Core.Diagnostics;
+
+public interface IDiagnosticsSink
+{
+    void LogInfo(string message);
+    void LogWarn(string message);
+    void LogError(string message, Exception? ex = null);
+}
