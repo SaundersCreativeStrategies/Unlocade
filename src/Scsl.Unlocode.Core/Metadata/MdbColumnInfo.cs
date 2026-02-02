1c362eea79aedfb31c559e4bd1a0b1d48ddb4160
asparatu <asparatu@gmail.com>
2026-02-02 22:16:03 +0800

    feat(core): add MDB column metadata entity

---
+++ src/Scsl.Unlocode.Core/Metadata/MdbColumnInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace Scsl.Unlocode.Core.Metadata;
+
+public sealed class MdbColumnInfo
+{
+    public string Name { get; init; } = default!;
+    public string DataType { get; init; } = default!;
+    public int? Size { get; init; }
+    public bool IsNullable { get; init; }
+}
