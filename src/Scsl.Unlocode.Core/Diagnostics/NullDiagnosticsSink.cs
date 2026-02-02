deb32ea761d449b26be010ba3620515063ae0e9c
asparatu <asparatu@gmail.com>
2026-02-02 22:11:22 +0800

    feat(core): add NullDiagnosticsSink

---
+++ src/Scsl.Unlocode.Core/Diagnostics/NullDiagnosticsSink.cs
@@ -0,0 +1,12 @@
+﻿namespace Scsl.Unlocode.Core.Diagnostics;
+
+public sealed class NullDiagnosticsSink : IDiagnosticsSink
+{
+    public static readonly IDiagnosticsSink Instance = new NullDiagnosticsSink();
+
+    private NullDiagnosticsSink() { }
+
+    public void LogInfo(string message) { }
+    public void LogWarn(string message) { }
+    public void LogError(string message, Exception? ex = null) { }
+}
