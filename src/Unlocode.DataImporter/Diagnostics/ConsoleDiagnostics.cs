15e584f6f7fd653da7878499e2bdddafcb19d7d5
asparatu <asparatu@gmail.com>
2026-02-03 19:30:55 +0800

    feat(diagnostics): add ConsoleDiagnostics to Unlocode.DataImporter.Diagnostics

---
+++ src/Unlocode.DataImporter/Diagnostics/ConsoleDiagnostics.cs
@@ -0,0 +1,36 @@
+﻿using Scsl.Unlocode.Core.Diagnostics;
+
+using Spectre.Console;
+
+namespace Unlocode.DataImporter.Diagnostics;
+
+public class ConsoleDiagnostics : IDiagnosticsSink
+{
+    private readonly bool _verbose;
+
+    public ConsoleDiagnostics(bool verbose)
+    {
+        _verbose = verbose;
+    }
+    public void LogInfo(string message)
+    {
+        if(_verbose)
+            AnsiConsole.MarkupLine($"[grey][Info]:[/] {message}");
+    }
+
+    public void LogWarn(string message)
+    {
+        if(_verbose)
+            AnsiConsole.MarkupLine($"[yellow][Warning]:[/] {message}");
+    }
+
+    public void LogError(string message, Exception? ex = null)
+    {
+        AnsiConsole.MarkupLine($"[red][Error]:[/] {message}");
+
+        if (_verbose && ex != null)
+        {
+            AnsiConsole.WriteException(ex);
+        }
+    }
+}
