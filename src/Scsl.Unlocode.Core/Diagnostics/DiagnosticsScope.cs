444fc4ca9567024d0a4de102818c7ad34bab2892
asparatu <asparatu@gmail.com>
2026-02-03 21:58:52 +0800

    feat(diagnostics): add diagnostics scope for total runtime measurement

---
+++ src/Scsl.Unlocode.Core/Diagnostics/DiagnosticsScope.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+namespace Scsl.Unlocode.Core.Diagnostics;
+
+public class DiagnosticsScope : IDisposable
+{
+    private readonly IDiagnosticsSink _sink;
+    private readonly DiagnosticsEventId _startEvent;
+    private readonly DiagnosticsEventId _completedEvent;
+    private readonly string _operation;
+    private readonly Stopwatch _stopwatch;
+
+    public DiagnosticsScope(
+        IDiagnosticsSink sink, DiagnosticsEventId startEvent,
+        DiagnosticsEventId completedEvent, string operation)
+    {
+        _sink = sink;
+        _completedEvent = completedEvent;
+        _startEvent = startEvent;
+        _operation = operation;
+
+        _sink.LogInfo(_startEvent, $"{operation} started.");
+        _stopwatch = Stopwatch.StartNew();
+    }
+
+    public void Dispose()
+    {
+       _stopwatch.Stop();
+
+       _sink.LogInfo(_completedEvent,
+           $"{_operation} completed in {_stopwatch.ElapsedMilliseconds} ms.");
+    }
+}
