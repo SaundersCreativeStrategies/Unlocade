3f46972e360596ac99d904d33e942789c064f119
asparatu <asparatu@gmail.com>
2026-02-03 23:56:16 +0800

    feat(mdb): add key and index metadata to MdbColumnInfo

--- src/Scsl.Unlocode.Core/Metadata/MdbColumnInfo.cs
+++ src/Scsl.Unlocode.Core/Metadata/MdbColumnInfo.cs
@@ -6,4 +6,12 @@ public sealed class MdbColumnInfo
     public string DataType { get; init; } = default!;
     public int? Size { get; init; }
     public bool IsNullable { get; init; }
+
+    public bool IsPrimaryKey { get; set; }
+    public bool IsIdexed { get; set; }
+    public bool IsUnique { get; set; }
+    public bool IsForeignKey { get; set; }
+    public string? ReferencesTable { get; set; }
+    public string? ReferencesColumn { get; set; }
+
 }
